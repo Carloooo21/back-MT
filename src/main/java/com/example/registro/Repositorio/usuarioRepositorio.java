b0b3feaa10d3d5678197c9cd33edc215be823543
carlopp21 <carlitosmancipito21@gmail.com>
2025-08-19 08:32:35 -0500

    Actualizacion con mi version local

--- src/main/java/com/example/registro/Repositorio/usuarioRepositorio.java
+++ src/main/java/com/example/registro/Repositorio/usuarioRepositorio.java
@@ -3,8 +3,6 @@ package com.example.registro.Repositorio;
 import com.example.registro.Entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface usuarioRepositorio extends JpaRepository<Usuario, Long> {
 
 
