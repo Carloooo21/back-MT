cc49e5e97485ac3496b71985977f04f203e7a1f5
carlopp21 <carlitosmancipito21@gmail.com>
2025-08-19 11:47:29 -0500

    Cambio cors url

--- src/main/java/CORS/CorsConfig.java
+++ src/main/java/CORS/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 // Corrección: Usar un solo dominio válido
-                .allowedOriginPatterns("https://mantenimiento-blue.vercel.app/")
+                .allowedOrigins("https://mantenimiento-blue.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
